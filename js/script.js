a1ed6f6e6d604282e146cdf9c4bb17fac99afe51
AX414 <joaovictorlisboaporcel@hotmail.com>
2024-03-20 10:59:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,73 +1,66 @@
 $(document).ready(function(){
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          
-          // Usando API de Geocode Reverso do OpenStreetMap para obter informações detalhadas de localização
-          var url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
-          $.getJSON(url, function(data) {
-              var cidade = data.address.city || data.address.town || data.address.village || data.address.hamlet;
-              var estado = data.address.state;
-              var pais = data.address.country;
-
-              var localizacao = "<ul>" +
-                                      "<li><b>Cidade:</b> " + (cidade || 'N/A') + "</li>" +
-                                      "<li><b>Estado:</b> " + (estado || 'N/A') + "</li>" +
-                                      "<li><b>País:</b> " + (pais || 'N/A') + "</li>" +
-                                      "<li><b>Latitude:</b> " + latitude + "</li>" +
-                                      "<li><b>Longitude:</b> " + longitude + "</li>" +
-                                  "</ul>";
-                
-              
-              $('#localizacao').html(localizacao);
-              
-              $('#btn').click(function() {
-                var mapUrl = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=15/${latitude}/${longitude}`;
-                var emailContent = `<h3>Nova Localização encontrada!</h3>
-                                    \n\n<b>Cidade:</b> ${cidade}
-                                    \n\n<b>Estado:</b> ${estado}
-                                    \n\n<b>País:</b> ${pais}
-                                    \n\n<b>Latitude:</b> ${latitude}
-                                    \n\n<b>Longitude:</b> ${longitude}
-                                    \n\n<b>Link do mapa:</b> ${mapUrl}`;
-                
-                  // Enviar e-mail usando EmailJS
-                  var serviceID = 'service_s1kj6pp'; // Substitua pelo seu Service ID do EmailJS
-                  var templateID = 'template_l40aacz'; // Substitua pelo seu Template ID do EmailJS
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            
+            // Usando API de Geocode Reverso do OpenStreetMap para obter informações detalhadas de localização
+            var url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+            $.getJSON(url, function(data) {
+                var cidade = data.address.city || data.address.town || data.address.village || data.address.hamlet;
+                var estado = data.address.state;
+                var pais = data.address.country;
+  
+                var localizacao = "<ul>" +
+                                        "<li><b>Cidade:</b> " + (cidade || 'N/A') + "</li>" +
+                                        "<li><b>Estado:</b> " + (estado || 'N/A') + "</li>" +
+                                        "<li><b>País:</b> " + (pais || 'N/A') + "</li>" +
+                                        "<li><b>Latitude:</b> " + latitude + "</li>" +
+                                        "<li><b>Longitude:</b> " + longitude + "</li>" +
+                                    "</ul>";
                   
-                  // Configurar o EmailJS com sua chave pública
-                  emailjs.init('dmcyCSlouymgq2U-m');
+                
+                $('#localizacao').html(localizacao);
+                
+                $('#btn').click(function() {
+                  var mapUrl = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=15/${latitude}/${longitude}`;
+                  
+                    // Enviar e-mail usando EmailJS
+                    var serviceID = 'service_s1kj6pp'; // Substitua pelo seu Service ID do EmailJS
+                    var templateID = 'template_l40aacz'; // Substitua pelo seu Template ID do EmailJS
                     
-                  // Enviar e-mail usando EmailJS
-                  emailjs.send(serviceID, templateID, { message: emailContent })
-                      .then(function(response) {
-                          console.log('E-mail enviado com sucesso:', response);
-                          alert('E-mail enviado com sucesso!');
-                          window.open(mapUrl);
-                      }, function(error) {
-                          console.error('Erro ao enviar e-mail:', error);
-                          alert('Erro ao enviar e-mail. Por favor, tente novamente mais tarde.');
-                      });
-              });
-          });
-      }, function(error) {
-          switch(error.code) {
-              case error.PERMISSION_DENIED:
-                  $('#localizacao').html("Permissão negada para acessar a localização.");
-                  break;
-              case error.POSITION_UNAVAILABLE:
-                  $('#localizacao').html("Informações de localização estão indisponíveis.");
-                  break;
-              case error.TIMEOUT:
-                  $('#localizacao').html("A solicitação para obter a localização do usuário expirou.");
-                  break;
-              case error.UNKNOWN_ERROR:
-                  $('#localizacao').html("Ocorreu um erro desconhecido ao tentar obter a localização do usuário.");
-                  break;
-          }
-      });
-  } else {
-      $('#localizacao').html("Geolocalização não é suportada por este navegador.");
-  }
-});
+                    // Configurar o EmailJS com sua chave pública
+                    emailjs.init('dmcyCSlouymgq2U-m');
+                      
+                    // Enviar e-mail usando EmailJS
+                    emailjs.send(serviceID, templateID, { cidade: cidade, estado: estado, pais: pais, latitude: latitude, longitude: longitude, mapUrl: mapUrl })
+                        .then(function(response) {
+                            console.log('E-mail enviado com sucesso:', response);
+                            alert('E-mail enviado com sucesso!');
+                            window.open(mapUrl);
+                        }, function(error) {
+                            console.error('Erro ao enviar e-mail:', error);
+                            alert('Erro ao enviar e-mail. Por favor, tente novamente mais tarde.');
+                        });
+                });
+            });
+        }, function(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    $('#localizacao').html("Permissão negada para acessar a localização.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    $('#localizacao').html("Informações de localização estão indisponíveis.");
+                    break;
+                case error.TIMEOUT:
+                    $('#localizacao').html("A solicitação para obter a localização do usuário expirou.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    $('#localizacao').html("Ocorreu um erro desconhecido ao tentar obter a localização do usuário.");
+                    break;
+            }
+        });
+    } else {
+        $('#localizacao').html("Geolocalização não é suportada por este navegador.");
+    }
+  });
